6fd988182a9ef63810bfe3aa73ee1fef7fba1533
ttru <truongtommy06@gmail.com>
2013-07-07 07:49:12 +0000

    tried to correct errors in web.js

--- web.js
+++ web.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var fs = require('fs');
-var buf = require('buffer');
+
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    response.send(buf.toString(fs.readFileSync("index.html")));
+    response.send(fs.readFileSync("index.html").toString());
 });
 
 var port = process.env.PORT || 5000;
