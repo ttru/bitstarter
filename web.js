ac5459283d3c054a6247faf6a894da5e4e06fa56
ttru <truongtommy06@gmail.com>
2013-07-07 07:34:34 +0000

    fixed typo: 'buf' to 'Buffer'

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    response.send(buf.toString(fs.readFileSync("index.html")));
+    response.send(Buffer.toString(fs.readFileSync("index.html")));
 });
 
 var port = process.env.PORT || 5000;
