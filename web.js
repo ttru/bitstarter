990d08186b14c269bf599ec6eadf66d18bfc5664
ttru <truongtommy06@gmail.com>
2013-07-07 07:41:48 +0000

    required buf

--- web.js
+++ web.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var fs = require('fs');
-
+var buf = require('buffer');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    response.send(Buffer.toString(fs.readFileSync("index.html")));
+    response.send(buf.toString(fs.readFileSync("index.html")));
 });
 
 var port = process.env.PORT || 5000;
